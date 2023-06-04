af7d9edb37e1e0ed451fa172985b72535d53a399
vda-777 <vda-777@ya.ru>
2023-06-04 14:53:57 +0300

    2023-06-04
    Вернул код в Modal, изменил ModalOverlay на один div передается один пропс на функцию закрытия, ловится кликом на нем и клавишей esc.
    В Modal передается пропсы header - заголовок окна и children - разметка содержимого окна.

--- src/components/modal-overlay/modal-overlay.jsx
+++ src/components/modal-overlay/modal-overlay.jsx
@@ -1,6 +1,5 @@
 //import { Overlay } from "react-portal-overlay";
 import React from 'react';
-import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import PropTypes from 'prop-types';
 import style from './modal-overlay.module.css';
 
@@ -16,30 +15,8 @@ export default function ModaOverlay(props) {
       ref={refModalOverlay}
       onClick={props.onClose}
       onKeyDown={(e)=>{if(e.key === 'Escape') props.onClose()}}
-    >
-        <div className={style.modalWindow}
-          onClick={(e)=>{e.stopPropagation()}}
-          //onKeyDown={(e)=>{e.stopPropagation()}}
-        >
-          
-          {
-            props.header === undefined 
-            ?
-              <div className={style.modalWindowHeaderNoCaption}>
-                  <span className='mt-10 ml-10'><p className="text text_type_digits-medium">{props.header}</p></span>
-                  <span className='mt-15 mr-10'><CloseIcon type="primary" onClick={props.onClose}/></span>
-              </div>
-            :
-              <div className={style.modalWindowHeaderCaption + ' mt-10'}>
-                    <p className="ml-10 text text_type_digits-medium">Детали ингредиента</p>
-                    <span className='mr-10'><CloseIcon type="primary" onClick={props.onClose}/></span>
-              </div>
-            }
-          <div className={style.modalWindowContent}>
-            {props.children === undefined ? <p className="text text_type_digits-medium">Просто окно ;-)</p> : props.children}
-          </div>
-        </div>        
-    </div>
+    />       
+    //</div>
   );    
 }
 
--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -2,15 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ModaOverlay from '../modal-overlay/modal-overlay';
 import {createPortal} from 'react-dom';
+import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import style from './modal.module.css';
+
 
 export default function Modal(props) {
   return (
     <>
         {createPortal(
-            <ModaOverlay header={props.header} onClose={props.onClose}>
-                {props.children}
-            </ModaOverlay>,
-            document.getElementById('modals')
+          <>
+            <ModaOverlay onClose={props.onClose}></ModaOverlay>
+            <div className={style.modalWindow}
+            //onClick={(e)=>{e.stopPropagation()}}
+            //onKeyDown={(e)=>{e.stopPropagation()}}
+            >
+        
+            {
+              props.header === undefined 
+              ?
+                <div className={style.modalWindowHeaderNoCaption}>
+                  <span className='mt-10 ml-10'><p className="text text_type_digits-medium">{props.header}</p></span>
+                  <span className='mt-15 mr-10'><CloseIcon type="primary" onClick={props.onClose}/></span>
+              </div>
+              :
+                <div className={style.modalWindowHeaderCaption + ' mt-10'}>
+                  <p className="ml-10 text text_type_digits-medium">Детали ингредиента</p>
+                  <span className='mr-10'><CloseIcon type="primary" onClick={props.onClose}/></span>
+              </div>
+            }
+              <div className={style.modalWindowContent}>
+                {props.children === undefined ? <p className="text text_type_digits-medium">Просто окно ;-)</p> : props.children}
+              </div>
+            </div>            
+          </>,
+            document.getElementById('modals')            
         )}
     </>
   );
